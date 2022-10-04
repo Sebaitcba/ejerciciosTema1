ad148ad980ca5c84162281c9b91dadf25941713b
Sebaitcba <sebasitnaarielruiz@gmail.com>
2022-10-04 08:53:37 -0300

    Primer ejercicio de Java

---
+++ src/tipoDatos/TiposDatos.java
@@ -0,0 +1,21 @@
+package tipoDatos;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+
+        //Declaración de variables
+        int entero = 7;
+        long largo = 7L;
+        double doble = 7D;
+        boolean siete = true;
+        String tipo = "7";
+
+        //Impresión de datos
+        System.out.println("Este es un entero: " + entero);
+        System.out.println("Este es un long: " + largo);
+        System.out.println("Este es un double: " + doble);
+        System.out.println("Este es un boolean: " + siete);
+        System.out.println("Este es un tipo: " + tipo);
+    }
+}
